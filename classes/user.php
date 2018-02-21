15ff8fc352cee566e0b11ef976f8ad8b65a4af24
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2018-02-21 16:31:43 +0100

    user validation

--- classes/user.php
+++ classes/user.php
@@ -9,5 +9,15 @@ class User {
     if($this->_db->insert('users', $fields)) {
        throw new Exception('There was a problem creating an account.');
     }
+  } public function find($user = null) {
+     if($user) {
+       $field = (is_numeric($user)) ? 'id' : 'username';
+       &data = this->_db->get('users', array($field, '=', user))
+     }
+  }
+  public function loggin($username, $password = null) {
+    $user = this->find($username);
+
+    return false
   }
 }
--- login.php
+++ login.php
@@ -3,12 +3,14 @@
   if(Input::exists()) {
     if(Token::check(Input::get('token'))) {
        $validate = new Validate();
-       $validation = $validate->check(array(
+       $validation = $validate->check($_POST,array(
          'username' => array('required' => true),
          'password' => array('required' => true),
        ))
        if($validation->passed()) {
          //log user in
+         $user = new User();
+         $login=$user->login(Input::get('username'),Input::get('password'));
                 } else {
                   foreach($validation->errors() as $errors) {
                     echo $error '<br>'
@@ -26,7 +28,7 @@
 
   div class="field">
   <label for="username">passwords</label>
-  <input type="text" name="password" id="password" autocomplete="off">
+  <input type="password" name="password" id="password" autocomplete="off">
   </div>
 
    <input type="hidden" value="token" value="<?php echo Token::generate(); ?>">
